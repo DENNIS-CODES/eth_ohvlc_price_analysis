75ee82795cd82d660c0c883888de1a03bc3f202d
Dennis Codes <dennis@ngeni.io>
2022-11-05 13:17:16 +0300

    implemented getting account info and positions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use dotenvy::dotenv;
 use ftx::{
     options::Options,
-    rest::{GetMarket, Rest, Result},
+    rest::{GetMarket, GetAccount, GetPositions, Rest, Result},
 };
 use tokio;
 
@@ -10,6 +10,11 @@ async fn main() -> Result<()> {
     dotenv().ok();
 
     let api = Rest::new(Options::from_env());
+    // Get account info and positions
+    println!("Account:");
+    println!("{:#?}", api.request(GetAccount {}).await.unwrap());
+    println!("Positions:");
+    println!("{:#?}", api.request(GetPositions {}).await.unwrap());
 
     let price = api.request(GetMarket::new("ETH-PERP")).await?.price;
     println!("ETH live price is {} USD.", price.unwrap());
